4c69c915fcafb5f7031afb22b8ab745c139f8c67
Houssem Achouri <houssem@3dverse.com>
2023-11-16 18:38:17 -0500

    Initial commit

--- config.js
+++ config.js
@@ -1,15 +1,6 @@
-// Find your Public Token in the 3dverse Console (https://console.3dverse.com):
-// - Go to the API Access page.
-// - Copy the Public Token and paste it below.
-
-export const publicToken = %YOUR_PUBLIC_TOKEN%;
-
-// Replace asset UUIDs:
-// - Go to the Asset Browser page.
-// - Double click on the Public folder to open it.
-// - Select a scene and copy the Asset UUID to replace it below.
-
-export const mainSceneUUID = %YOUR_MAIN_SCENE_UUID%;
-export const characterControllerSceneUUID = %YOUR_CHARACTER_CONTROLLER_SCENE_UUID%;
-
+export const publicToken = "public_wDCpSOFjnbpxWKiT";
+export const mainSceneUUID = "656641e6-46f4-4017-8fae-9548a675b195";
+export const characterControllerSceneUUID =
+  "39321a40-5176-4fed-83a6-c328a3f648b4";
 export const spawnPosition = [0,0,0];
+
